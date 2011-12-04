2825b1c9179be51cf7dd2e58271f525d34d5cf9b
Scott Hillman <hillmanov+github@gmail.com>
2011-12-03 20:22:55 -0700

    Can now give the DBManager a reference to a schema file insteat of a sqlite database

--- com/covenant/DbManager.java
+++ com/covenant/DbManager.java
@@ -1,9 +1,11 @@
 package com.covenant;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import android.content.Context;
@@ -12,26 +14,56 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbManager extends SQLiteOpenHelper {
 
+	
+	public static final int DB = 0;
+	public static final int SCHEMA = 1;
+	
 	private String dbPath;
 	private String dbName;
 	private static final Integer DB_VERSION = 1;
 	private final Context context;
 	private SQLiteDatabase db;
 
-	public DbManager(Context context, String dbName) {
-		super(context, dbName, null, DB_VERSION);
+	public DbManager(Context context, int mode, String resource) {
+		super(context, resource, null, DB_VERSION);
 		this.context = context;
-		this.dbName = dbName;
-		this.dbPath = "/data/data/" + context.getPackageName() + "/databases/";
-		if (!databaseExistsOnDevice()) {
-			copyDatabase();
+		this.dbName = resource;
+		
+		switch (mode) {
+		case DB:
+			this.dbPath = "/data/data/" + context.getPackageName() + "/databases/";
+			if (!databaseExistsOnDevice()) {
+				copyDatabase();
+			}
+			db = context.openOrCreateDatabase(resource, Context.MODE_PRIVATE, null);
+			break;
+		case SCHEMA:
+			db = context.openOrCreateDatabase("data.sqlite", SQLiteDatabase.CREATE_IF_NECESSARY, null);
+			
+			try {
+				BufferedReader r;
+				r = new BufferedReader(new InputStreamReader(context.getAssets().open(resource)));
+				StringBuilder schemaAll = new StringBuilder();
+				String line;
+				while ((line = r.readLine()) != null) {
+					schemaAll.append(line);
+				}
+				
+				String[] statements = schemaAll.toString().split(";");
+				for (String statement : statements) {
+					db.execSQL(statement);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			break;
 		}
-		db = context.openOrCreateDatabase(dbName, Context.MODE_PRIVATE, null);
 	}
 
 	private boolean databaseExistsOnDevice() {
 		File dbFile = new File(dbPath + dbName);
-		return dbFile.exists();
+		return dbFile.exists();	
 	}
 
 	private void copyDatabase() {
@@ -49,7 +81,7 @@ public class DbManager extends SQLiteOpenHelper {
 			dbOut.close();
 			assetsDB.close();
 		} catch (IOException e) {
-
+			System.out.println(e.getStackTrace());
 		}
 	}
 
